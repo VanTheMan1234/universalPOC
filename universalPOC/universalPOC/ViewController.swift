c8f5bf2939b8b32ceb6af6338e546bbeb83614a8
Vanoshan Ramdhani <vanoshan1234@gmail.com>
2020-04-07 23:35:23 +0200

    Uploaded Files

---
+++ universalPOC/universalPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  universalPOC
+//
+//  Created by Vanoshan  Ramdhani on 2020/04/07.
+//  Copyright © 2020 Vanoshan  Ramdhani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
